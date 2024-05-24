7ba6c9a9d5e1ba5f205ef2f813ba22c902cc7593
Efa-Isnawati <efaisnawati1911@gmail.com>
2024-05-24 10:40:55 +0700

    fix: done project nextjs1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "cdn2.thecatapi.com",
+      "image.tmdb.org",
+      "encrypted-tbn0.gstatic.com",
+    ],
+  },
+};
 
 export default nextConfig;
---
+++ src/app/data/data.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    id: 1,
+    name: "Whiskers",
+    type: "cat",
+    description: "A fluffy white cat.",
+    img: "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg",
+  },
+  {
+    id: 2,
+    name: "The Matrix",
+    type: "film",
+    description: "A sci-fi action film.",
+    img: "https://image.tmdb.org/t/p/w500/8mRgpubxHqnqvENK4Bei30xMDvy.jpg",
+  },
+  {
+    id: 3,
+    name: "My Laptop",
+    type: "product",
+    description: "Hp merk",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxjHCVw3Qe59dgWH2i2pmfyEUL8b2ljF_0jQ&usqp=CAU",
+  },
+  {
+    id: 4,
+    name: "Sponge Bob",
+    type: "character",
+    description: "Spongebob cartoon.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj4fzIq0fr1jhfkJ7eeoMCiuPOgZCG0cbAnA&usqp=CAU",
+  },
+];
+
+export default data;
